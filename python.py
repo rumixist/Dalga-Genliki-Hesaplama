b2f76292a7d67ecacf94e5a9078c5e32f97e9252
rumixist <133505669+rumixist@users.noreply.github.com>
2024-04-28 14:13:30 +0300

    Update python.py

--- python.py
+++ python.py
@@ -2,7 +2,7 @@ import math
 
 # Verilen değerler
 f = 1e6  # Frekans (1 MHz)
-E = 1  # Enerji (1 Joule)
+E = 4014.4  # Enerji (1 Joule)
 c = 3e8  # Işık hızı (m/s)
 G = 6.674e-11  # Kütleçekim sabiti (m^3/kg/s^2)
 lambd = 1.616e-35  # Planck uzunluğu (m)
